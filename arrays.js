c8a7d9bd19d504575e13d97bf7c8dd21b7e16bd3
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 17:43:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,4 +20,32 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
     return array
 }
 
+unction destructivelyAddElementToEndOfArray(array, element) {
+    array.push(element);
+    return array
+}
+
+function accessElementInArray(array, index) {
+    return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+    array.shift()
+    return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+    var new_array = array.slice(1)
+    return new_array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+    array.pop()
+    return array
+}
+
+function removeElementFromEndOfArray(array) {
+    var new_array = array.slice(0, array.length - 1)
+    return new_array
+}
 
