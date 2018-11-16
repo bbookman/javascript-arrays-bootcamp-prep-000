9591f9282542d4e21c10d34caf468b80d17b4bda
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 17:27:54 +0000

    automatically backed up by learn

---
+++ test.js
@@ -0,0 +1,8 @@
+function addElementToEndOfArray(array, element) {
+  array.push(element)
+  log.console(array)
+  return array
+}
+
+somestuff =["Russell", "Benetton"]
+addElementToEndOfArray(somestuff,"hello")
